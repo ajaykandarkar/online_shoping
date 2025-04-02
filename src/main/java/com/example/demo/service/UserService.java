04347faa4e1be4510b51ecb0a87ac2a05b5ab18c
Rgaherwar <114740471+RahulGaherwar6@users.noreply.github.com>
2025-04-02 21:38:21 +0530

    Update UserService.java

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
